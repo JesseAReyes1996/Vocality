a736d8484f7a5dbd89ef1dea8ce4683bec88a05b
Jesse <jreye039@ucr.edu>
2019-01-26 21:26:38 -0800

    finished login functionality

--- app/src/main/java/edu/jreye039/vocality/LogIn.java
+++ app/src/main/java/edu/jreye039/vocality/LogIn.java
@@ -8,26 +8,24 @@ import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
+    EditText usernameEditText;
+    EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        Button signInBtn = (Button) findViewById(R.id.signInBtn);
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Get log in info
-                EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
-                EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+    }
 
-                //Cast the info to strings
-                String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+    public void OnLogin(View view) {
+        //get login info
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
-                BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-                backgroundWorker.execute("login", username, password);
-            }
-        });
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute("login", username, password);
     }
 }
