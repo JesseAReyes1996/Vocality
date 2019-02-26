4c11eae68d2d89c8ff0b0f5b2ab6ec667dad74c0
Jesse <jreye039@ucr.edu>
2019-02-26 01:10:20 -0800

    started main feed

---
+++ app/src/main/java/edu/jreye039/vocality/FeedAdapter.java
@@ -0,0 +1,52 @@
+package edu.jreye039.vocality;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder> {
+
+    private ArrayList<FeedItem> mFeedList;
+
+    public static class FeedViewHolder extends RecyclerView.ViewHolder {
+        public TextView mCoverTitle;
+        public TextView mUser;
+
+        public FeedViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mCoverTitle = itemView.findViewById(R.id.coverTitleTextView);
+            mUser = itemView.findViewById(R.id.coverAuthorTextView);
+        }
+    }
+
+    public FeedAdapter(ArrayList<FeedItem> feedList){
+        mFeedList = feedList;
+    }
+
+    @NonNull
+    @Override
+    public FeedViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.feed_item, viewGroup, false);
+        FeedViewHolder fvh = new FeedViewHolder(v);
+        return fvh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedViewHolder feedViewHolder, int i) {
+        FeedItem currentItem = mFeedList.get(i);
+
+        feedViewHolder.mCoverTitle.setText(currentItem.getmCoverTitle());
+        feedViewHolder.mUser.setText("covered by " + currentItem.getmUsername());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFeedList.size();
+    }
+}
--- app/src/main/java/edu/jreye039/vocality/FeedFragment.java
+++ app/src/main/java/edu/jreye039/vocality/FeedFragment.java
@@ -4,14 +4,48 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class FeedFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+        // 1. get a reference to recyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.feedRecyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));//getActivity().getApplicationContext()
+
+        // this is data for recycler view
+        ArrayList<FeedItem> exampleList = new ArrayList<>();
+        exampleList.add(new FeedItem("Car Man Driver Guy","cornflower"));
+        exampleList.add(new FeedItem("H","creeper"));
+        exampleList.add(new FeedItem("See Ya Later Boi","greg"));
+        exampleList.add(new FeedItem("Upside Down Mowgu","jaja"));
+        exampleList.add(new FeedItem("Faded","jesse"));
+        exampleList.add(new FeedItem("It's My Destiny","lastfire"));
+        exampleList.add(new FeedItem("Dancing Keyblade","lyn"));
+        exampleList.add(new FeedItem("Knight Man","mark"));
+        exampleList.add(new FeedItem("Teemo Time","mp3"));
+        exampleList.add(new FeedItem("Self Improvement Shriek","nerve"));
+        exampleList.add(new FeedItem("Plant Man Murder Beard","toastus"));
+
+
+        // 3. create an adapter
+        FeedAdapter mFeedAdapter = new FeedAdapter(exampleList); //TODO move this to a background worker eventually
+
+        // 4. set adapter
+        recyclerView.setAdapter(mFeedAdapter);
+
+        return rootView;
     }
 }
