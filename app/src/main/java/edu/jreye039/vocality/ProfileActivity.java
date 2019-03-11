f3f136c1c03d926c8db12e9af3c8e72114d95b25
谢奇光 <xieqiguang@ucrwpa-1-8-10-25-29-252.wnet.ucr.edu>
2019-03-11 15:28:35 -0700

    upload images

--- app/src/main/java/edu/jreye039/vocality/ProfileActivity.java
+++ app/src/main/java/edu/jreye039/vocality/ProfileActivity.java
@@ -1,30 +1,50 @@
 package edu.jreye039.vocality;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.net.UrlQuerySanitizer;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 import org.apache.http.params.HttpParams;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
     private static final int RESULT_LOAD_IMAGE = 1;
@@ -33,6 +53,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     Button bUploadImage, bDownloadImage;
     TextView nameTextView;
     private RecyclerView RecordRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +86,28 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         ProfileBackgroundWorker backgroundWorker = new ProfileBackgroundWorker(this, RecordRecyclerView);
         backgroundWorker.execute(username);
 
+        s3_key = getIntent().getStringExtra("AWS_S3_KEY");
+
+        //create the temp file
+        outputDir = this.getCacheDir();
+        try {
+            tempFile = File.createTempFile("temp", ".jpg", outputDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //download the file
+        credentialsProvider();
+
+        setTransferUtility();
+
+        //downloadFileFromS3();
+
+        //check if the user has allowed Vocality to access their storage/mic
+        if (!checkPermissionFromDevice()) {
+            requestPermission();
+        }
+
     }
 
     @Override
@@ -72,7 +115,26 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()){
 
             case R.id.bUploadImage:
-                Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
+                if(s3Upload == null || !s3Upload.exists()){
+                    Log.d("AWS S3 UPLOAD", "FILE NOT FOUND");
+                }
+                else{
+                    //upload the file to AWS S3
+                    credentialsProvider();
+                    setTransferUtility();
+                    uploadFileToS3();
+
+                    //get the user's username
+                    SharedPreferences userInfo = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+                    String username = userInfo.getString("username", "");
+
+                    //attach the recording to the user
+                    String title = s3_key.substring(0, s3_key.length() - 4);
+                    ImageBackgroundWorker backgroundWorker = new ImageBackgroundWorker(getApplicationContext());
+                    backgroundWorker.execute(username, title, fileKey, s3_key);
+                    
+                }
+                //Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
 
                 break;
             //case R.id.bDownloadImage:
@@ -82,6 +144,12 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.imageToUpload:
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                String fileID = UUID.randomUUID().toString();
+                pathSave = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + fileID + "_image.jpg";
+                s3Upload = new File(pathSave);
+                fileKey = fileID + "_image.jpg";
+
+                //Toast.makeText(ProfileActivity.this, "Recording...", Toast.LENGTH_SHORT).show();
                 break;
 
         }
@@ -97,36 +165,91 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    /*private class UploadImage extends AsyncTask<Void, Void, Void>{
-        Bitmap image;
-        String name;
 
-        public UploadImage(Bitmap image, String name){
-            this.image = image;
-            this.name = name;
-        }
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-        }
+    String pathSave = "";
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+    //the AWS S3 link where the backing track is stored
+    String s3_key;
 
-            ArrayList<Pair<String, String>> dataToSend = new ArrayList<>();
-            dataToSend.add(new Pair<String, String>("image", encodedImage));
-            dataToSend.add(new Pair<String, String>("name", name));
+    //temp file/directory to store the S3 object
+    File outputDir;
+    File tempFile;
 
-            return null;
+    final int REQUEST_PERMISSION_CODE = 1000;
+
+    private void requestPermission(){
+        //TODO might not need to read external storage, maybe take out
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, REQUEST_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        switch (requestCode)
+        {
+            case REQUEST_PERMISSION_CODE:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){}
+                else{
+                    Toast.makeText(this, "Permission Denied: Please allow Vocality to access the device's microphone/storage", Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
         }
     }
 
-    private Htt getHttpRequestParams(){
-        HttpParams httpRequestParams = new BasicHttpParams();
-    }*/
+    private boolean checkPermissionFromDevice(){
+        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return write_external_storage_result == PackageManager.PERMISSION_GRANTED && record_audio_result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    //AWS S3
+    AmazonS3Client s3;
+    TransferUtility transferUtility;
+    File s3Upload;
+    String fileKey;
+
+    public void credentialsProvider(){
+        //initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-west-2:af04cf4e-54ef-4854-97aa-8613ba65e9a9", //identity pool ID
+                Regions.US_WEST_2 // Region
+        );
+        setAmazonS3Client(credentialsProvider);
+    }
+
+    public void setAmazonS3Client(CognitoCachingCredentialsProvider credentialsProvider){
+        //create an S3 client
+        s3 = new AmazonS3Client(credentialsProvider);
+
+        //set the region of your S3 bucket
+        s3.setRegion(Region.getRegion(Regions.US_WEST_2));
+
+    }
+
+    public void setTransferUtility(){
+
+        transferUtility = new TransferUtility(s3, getApplicationContext());
+    }
+
+    public void uploadFileToS3(){
+
+        TransferObserver transferObserver = transferUtility.upload(
+                "vocality", /* The bucket to upload to */
+                fileKey,           /* The file's name on S3 */
+                s3Upload           /* The file to be uploaded's local location */
+        );
+    }
+
+    public void downloadFileFromS3(){
+
+        TransferObserver transferObserver = transferUtility.download(
+                "vocality", /* The bucket to download from */
+                s3_key,            /* The key for the object to download */
+                tempFile           /* The file to download the object to */
+        );
+    }
 
 }
 
